164be978387473986c43ff1b3fd53510b0e24b9f
yjhn <andrius.puksta@gmail.com>
2022-11-26 11:28:40 +0200

    remove dead code

--- main.py
+++ main.py
@@ -2,53 +2,6 @@ from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk
 
-class FeetToMeters:
-    
-    def __init__(self, root):
-        root.title("Feet to Meters")
-
-        # Window setup.
-        self.mainframe = ttk.Frame(root, padding="3 3 12 12")
-        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        # Make frame expand to fit the window if it is resized.
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-        
-        # Image
-        self.img1 = ImageTk.PhotoImage(file="test_image_1.jpg")
-        self.img1_label = ttk.Label(self.mainframe)
-        self.img1_label.grid(column=1, row=1, sticky=W)
-        self.img1_label["image"] = self.img1
-
-        # Feet input widget.
-        self.feet = StringVar()
-        feet_entry = ttk.Entry(self.mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-        # Meters label.
-        self.meters = StringVar()
-        ttk.Label(self.mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
-
-        # "Calculate" button.
-        ttk.Button(self.mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
-
-        ttk.Label(self.mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(self.mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(self.mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-        for child in self.mainframe.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
-
-    def calculate(self, *args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
-        except ValueError:
-            pass
-
 class Taylor:
     
     def __init__(self, root):
@@ -116,6 +69,5 @@ class Taylor:
         self.output_img_dest_fn["image"] = self.dest_fn_img
 
 root = Tk()
-# FeetToMeters(root)
 Taylor(root)
 root.mainloop()
